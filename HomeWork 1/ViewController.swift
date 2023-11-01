7ce642dc2e2ad6d59146b1bf07f7fe5bafc888ef
Valeroony <ssorento1@mail.ru>
2023-11-01 22:31:59 +0300

    Initial Commit

---
+++ HomeWork 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork 1
+//
+//  Created by валерон on 01.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
